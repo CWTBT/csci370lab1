298d1a9ab5810466fd7842b1e01e87b5741ee8a5
Matthew Kalahiki <kalahikimw@hendrix.edu>
2021-02-02 21:07:48 -0600

    Ranomly spawns pancakes from the sky

---
+++ Assets/Scripts/DropPancakes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropPancakes : MonoBehaviour
+{
+
+    public GameObject pancake;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("DropPancake2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+ 
+    IEnumerator DropPancake2()
+    {
+        yield return new WaitForSeconds(2f);
+        for (; ; )
+        {
+            Instantiate(pancake);
+
+            pancake.transform.position = new Vector2(Random.Range(-5,5),pancake.transform.position.y);
+
+            yield return new WaitForSeconds(2.5f);
+        }
+    }
+}
