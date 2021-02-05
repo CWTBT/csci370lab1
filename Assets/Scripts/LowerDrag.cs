3e16f7ae2cc6938feb4c30c803d0af2d048ae583
Matthew Kalahiki <kalahikimw@hendrix.edu>
2021-02-04 18:19:45 -0600

    pancakes fall slower, pancake hinges have limits, higher friction

---
+++ Assets/Scripts/LowerDrag.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowerDrag : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.drag = 0;
+
+    }
+}
